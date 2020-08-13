f68b58f020bd00f9a2a86b709ee3ced34b229761
zsh git_setup.sh <leonie.elody@gmail.com>
2020-08-13 21:19:01 +0200

    k

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,11 +1,14 @@
 class DosesController < ApplicationController
   before_action :find_cocktail, only: [:new, :create]
+
   def new
     @dose = Dose.new
   end
+
   def show
     @dose = Dose.find(params[:id])
   end
+
   def create
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
@@ -15,15 +18,19 @@ class DosesController < ApplicationController
       render :new
     end
   end
+
   def destroy
-      @dose = Dose.find(params[:id])
-      @dose.destroy
-      redirect_to cocktail_path(@dose.cocktail)
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
+
   private
+
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
   end
+
   def find_cocktail
     @cocktail = Cocktail.find(params[:cocktail_id])
   end
